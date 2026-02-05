6052acd715761e344f780131d840d2580c3ec4c9
Kirill Khorun <agoravobia@gmail.com>
2026-02-05 15:14:22 +0200

    up

---
+++ keyboards/crkbd_rp2040/rules.mk
@@ -0,0 +1,10 @@
+# Bootloader selection
+MCU = RP2040
+BOOTLOADER = rp2040
+
+SERIAL_DRIVER = vendor
+MOUSEKEY_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+TRI_LAYER_ENABLE = yes
+CAPS_WORD_ENABLE = yes    # Для Caps Word через комбо
+#LTO_ENABLE = yes
--- keyboards/hworne/rules.mk
+++ keyboards/hworne/rules.mk
@@ -1,16 +1,10 @@
 # Bootloader selection
 MCU = RP2040
 BOOTLOADER = rp2040
-BOOTMAGIC_ENABLE = yes
-SPLIT_KEYBOARD = yes
+
 SERIAL_DRIVER = vendor
-SRC += combos.c
-TAP_DANCE_ENABLE = yes
-VIA_ENABLE = yes
-COMBO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 TRI_LAYER_ENABLE = yes
-EXTRAKEY_ENABLE = yes        # Для медиа-клавиш
 CAPS_WORD_ENABLE = yes    # Для Caps Word через комбо
 BACKLIGHT_ENABLE = yes
